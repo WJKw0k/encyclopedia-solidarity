c34de337eed2106221e10862522f5d7920d9e216
chelojimenez <marcelojimenezrocabado@gmail.com>
2022-04-18 15:55:08 -0700

    create abbreviations dict

--- preprocess.py
+++ preprocess.py
@@ -1,7 +1,19 @@
-import spacy
+from string import printable
+import pandas as pd
 
+def get_abvs():
+    # open a txt
+    abvs = {}
+    with open('solidarity.abbreviations.txt', 'r') as f:
+        for line in f:
+            line = line.strip()
+            if line:
+                line = line.split(' * ')
+                abvs[line[0].strip()] = line[1].strip()
+    return abvs
 def main():
-    pass
+    abvs  = get_abvs()
+    print(abvs)
 
 if __name__ == '__main__':
     main()
