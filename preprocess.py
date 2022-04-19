e3d05d4aaab8a670e460fa6d97f1e96770252e21
chelojimenez <marcelojimenezrocabado@gmail.com>
2022-04-19 14:27:52 -0700

    added json file

--- preprocess.py
+++ preprocess.py
@@ -1,5 +1,6 @@
 from string import printable
 import pandas as pd
+import json
 
 def get_abvs():
     # open a txt
@@ -11,9 +12,39 @@ def get_abvs():
                 line = line.split(' * ')
                 abvs[line[0].strip()] = line[1].strip()
     return abvs
-def main():
+
+def json_to_csv(json_file):
     abvs  = get_abvs()
-    print(abvs)
+    # create a dataframe
+    sents = []
+    # read json as dict
+    with open(json_file, 'r') as f:
+        data = json.load(f)
+    for key in data:
+        # add data[key] to df
+        bio = data[key]['bio']
+        # erase last sent
+        bio = bio[:-1]
+        bio = [sent for sent in bio if sent]
+        for sent in bio:
+            sent = sent.strip()
+            # if not empty
+            if len(sent) > 0:
+                print(len(sent), sent)
+                if len(sent) == 0:
+                    print(len(sent))
+                for abv in abvs:
+                    sent = sent.replace(abv, abvs[abv])
+                sents.append(sent)
+    # convert sents to csv
+    df = pd.DataFrame(sents)
+    # rename col
+    df.columns = ['sentence']
+    df.to_csv('biogramy_info.csv', index=False)
+def main():
+    
+    json_to_csv('biogramy_info.json')
+
 
 if __name__ == '__main__':
     main()
